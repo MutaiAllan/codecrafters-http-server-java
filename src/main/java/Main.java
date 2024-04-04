d0e11b8ba3578d23651767305bb61a59636ff8bd
MutaiAllan <allankiprop175@gmail.com>
2024-04-04 17:58:19 +0300

    Added response

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,43 +1,36 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.DataOutputStream;
-import java.util.Objects;
-import java.io.OutputStream;
 
+import java.util.Objects;
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
     ServerSocket serverSocket = null;
     Socket clientSocket = null;
-    
     try {
       serverSocket = new ServerSocket(4221);
       serverSocket.setReuseAddress(true);
+      System.out.println("Waiting for connection!");
       clientSocket = serverSocket.accept(); // Wait for connection from client.
-      DataOutputStream dataOutputStream =
-          new DataOutputStream(clientSocket.getOutputStream());
-      dataOutputStream.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-      dataOutputStream.flush();
-      System.out.println("accepted new connection");
-
       System.out.println("Connected!");
       try (BufferedReader in = new BufferedReader(
                new InputStreamReader(clientSocket.getInputStream()))) {
         try (OutputStream out = clientSocket.getOutputStream()) {
-
-        String[] request =
-            in.readLine().split(" "); // Ignore the client input
-        if (Objects.equals(request[1], "/")) {
-          out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-        } else {
-          out.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+          String[] request =
+              in.readLine().split(" "); // Ignore the client input
+          if (Objects.equals(request[1], "/")) {
+            out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+          } else {
+            out.write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+          }
         }
-    }
+      }
+      System.out.println("accepted new connection");
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
